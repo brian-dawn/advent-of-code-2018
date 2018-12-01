a3b6847435e14781b1f7c95194ecb120fa09c20f
Brian Dawn <brian.t.dawn@gmail.com>
2018-12-01 08:18:56 -0600

    Add better error handling.

--- src/day1.rs
+++ src/day1.rs
@@ -1,30 +1,32 @@
+use failure::bail;
+use failure::Error;
 use itertools::FoldWhile::{Continue, Done};
 use itertools::Itertools;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-use std::collections::HashSet;
-
-fn one() {
-    let input = File::open("input/day1-1.txt").unwrap();
+fn read_input() -> Result<Vec<i32>, Error> {
+    let input = File::open("input/day1-1.txt")?;
     let buffered = BufReader::new(input);
 
-    let answer: i32 = buffered
+    // Collect will take our iter of results and make it
+    // a result of the inner thing, neat.
+    buffered
         .lines()
-        .map(|line| line.unwrap().parse::<i32>().unwrap())
-        .sum();
-
-    println!("1-1: {}", answer);
+        .map(|line| Ok(line?.parse::<i32>()?))
+        .collect()
 }
 
-fn two() {
-    let input = File::open("input/day1-1.txt").unwrap();
-    let buffered = BufReader::new(input);
+fn one() -> Result<(), Error> {
+    let answer: i32 = read_input()?.iter().sum();
+    println!("1-1: {}", answer);
+    Ok(())
+}
 
-    let (answer, _) = buffered
-        .lines()
-        .map(|line| line.unwrap().parse().unwrap())
-        .collect::<Vec<i32>>()
+fn two() -> Result<(), Error> {
+    let input = read_input();
+    let (answer, _) = input?
         .iter()
         .cycle()
         .fold_while((0, HashSet::new()), |(current_freq, mut observed), val| {
@@ -36,9 +38,10 @@ fn two() {
             }
         }).into_inner();
     println!("1-2: {}", answer);
+    Ok(())
 }
 
 pub fn run() {
-    one();
-    two();
+    one().unwrap();
+    two().unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate failure;
 extern crate itertools;
 
 mod day1;
